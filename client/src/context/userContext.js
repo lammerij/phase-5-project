bda9fb2944304eadbcf4459d93c148556dd7cd45
+ <+>
2023-01-24 14:11:48 -0500

    add user context and styled comp

---
+++ client/src/context/userContext.js
@@ -0,0 +1,12 @@
+import React, { createContext, useState } from "react";
+
+const UserContext = createContext();
+
+function UserProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const value = [user, setUser];
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+}
+
+export { UserContext, UserProvider };
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   resources :donations
   resources :fundraisers
   resources :causes
+  
+
+  # User Routes 
   resources :users
+  get "/auth", to: "users#show"
+  post "/signup", to: "users#create"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # get '/hello', to: 'application#hello_world'
   # Defines the root path route ("/")
