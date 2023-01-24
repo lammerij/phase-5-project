e80ac820eb6c4d795d8c887ac9b7944d0c490554
+ <+>
2023-01-24 13:06:25 -0500

    updated sessions, action, users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+  skip_before_action :authorize, only: [:create]
+
+  def index
+    users = User.all
+    render json: users
+  end
+
+  def show
+    render json: @current_user
+  end
+
+  def create
+    new_user = User.create(user_params)
+    if new_user.valid?
+      session[:new_user] = new_user.id
+      render json: new_user, status: :created
+    else
+      render json: { errors: ["user_data_invalid"] }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:id, :username, :display_name, :password, :type)
+  end
 end
