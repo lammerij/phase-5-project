c8a109aadcbf7b4bd08927c943fa258c11a15bf2
+ <+>
2023-01-24 12:50:59 -0500

    add associations and user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
-    has_secure_password
+  has_secure_password
+
+  validates :username, presence: true, uniqueness: true, length: { minimum: 4 }
+  validates :display_name, presence: true, uniqueness: true, length: { minimum: 8 }
+  validates :password, length: { in: 6..20 }
+  validates :password_confirmation, length: { in: 6..20 }
+  validates :type, presence: true
 end
